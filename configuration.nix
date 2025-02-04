c86cefc372137b15b0c21191613ad22f212d8b13
Ren <renmain@proton.me>
2025-02-03 21:39:26 -0600

    asdf

--- configuration.nix
+++ configuration.nix
@@ -1,58 +1,18 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+{ config, pkgs, inputs, ... }: {
 
-{ config, pkgs, inputs, ... }:
+#{{{ Basic Stuff
+imports = [
+    ./hardware-configuration.nix
+    inputs.sops-nix.nixosModules.sops
+];
+system.stateVersion = "24.11"; # Did you read the comment?
+nix.settings.experimental-features = [ "nix-command" "flakes" ];
+nixpkgs.config.allowUnfree = true;
+time.timeZone = "America/Chicago";
+#{{{ Locale
+i18n.defaultLocale = "en_US.UTF-8";
 
-{
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      inputs.sops-nix.nixosModules.sops
-    ];
-  sops = {
-  defaultSopsFile = ./secrets.yaml;
-  validateSopsFiles = false;
-  secrets.machine-password.neededForUsers = true;
-  secrets.cloudflare-token = {};
-  templates."ddclient.conf".content = ''
-    cache=/var/lib/ddclient
-    usev4=webv4
-    webv4=dynamicdns.park-your-domain.com/getip
-    protocol=cloudflare, \
-    zone=wren-homepage.online, \
-    login=token, \
-    password="${config.sops.placeholder.cloudflare-token}" \
-    laptop.wren-homepage.online
-  '';
-      age = {
-      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-      keyFile = "/var/lib/sops-nix/key.txt";
-      generateKey = true;
-    };
-  };
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "ren-laptop"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "America/Chicago";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
+i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
     LC_MEASUREMENT = "en_US.UTF-8";
@@ -62,149 +22,139 @@
     LC_PAPER = "en_US.UTF-8";
     LC_TELEPHONE = "en_US.UTF-8";
     LC_TIME = "en_US.UTF-8";
-  };
+};
+#}}}
+#}}}
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+#{{{ Secrets
+sops = {
+    defaultSopsFile = ./secrets.yaml;
+    validateSopsFiles = false;
+    age = {
+        sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+        keyFile = "/var/lib/sops-nix/key.txt";
+        generateKey = true;
+    };
+};
+#}}}
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+#{{{ Bootloader
+boot.loader.systemd-boot.enable = true;
+boot.loader.efi.canTouchEfiVariables = true;
+#}}}
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
+#{{{ Networking
+networking = {
+    hostName = "ren-laptop"; # Define your hostname.
+    networkmanager.enable = true;
+    firewall.allowedTCPPorts = [ 22 53 ];
+    firewall.allowedUDPPorts = [ 53 ];
+};
+#}}}
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  security.rtkit.enable = true;
-  services.pipewire = {
+#{{{ Desktop Environment 
+services.xserver = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  services.openssh = {
-  enable = true;
-  ports = [ 22 ];
-  settings = {
-    PasswordAuthentication = true;
-    AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
-    UseDns = true;
-    X11Forwarding = false;
-    PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
-  };
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+    xkb = {
+        layout = "us";
+        variant = "";
+    };
 };
-  services.fail2ban = {
-     enable = true;
-     maxretry = 5;
-         ignoreIP = [
-      "10.0.0.0/8" "172.16.0.0/12" "192.168.0.0/16"
-    ];
-    bantime = "72h"; # Ban IPs for one day on the first ban
+services.displayManager.autoLogin = {
+    enable = true;
+    user = "ren";
 };
 
-  services.ddclient = {
-    enable = false;
-    configFile = config.sops.templates."ddclient.conf".path;
-    
-  };
-  services.tailscale.enable = false;
-  services.cloudflare-dyndns = {
+#}}}
+
+#{{{ Services
+security.rtkit.enable = true;
+services = {
+    printing.enable = true;
+    pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+    };
+    openssh = {
+        enable = true;
+        ports = [ 22 ];
+        settings = {
+            PasswordAuthentication = true;
+            AllowUsers = [ "ren" ];
+            UseDns = true;
+            X11Forwarding = false;
+            PermitRootLogin = "no";
+        };
+    };
+    fail2ban = {
+        enable = true;
+        maxretry = 5;
+        ignoreIP = [
+            "10.0.0.0/8" "172.16.0.0/12" "192.168.0.0/16"
+        ];
+        bantime = "72h";
+    };
+    tailscale.enable = false;
+};
+
+sops.secrets.cloudflare-token = {};
+services.cloudflare-dyndns = {
     enable = false;
     domains = [ "laptop.wren-homepage.online" ];
     apiTokenFile = config.sops.secrets.cloudflare-token.path;
-  };
+};
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.mutableUsers = false;
-  users.users.ren = {
-    isNormalUser = true;
-    description = "Ren";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
-    hashedPasswordFile = config.sops.secrets.machine-password.path;
-  };
-  users.users.root = {
-    hashedPasswordFile = config.sops.secrets.machine-password.path;
+#}}}
 
-  };
+#{{{ Users
+sops.secrets.machine-password.neededForUsers = true;
+users = {
+    mutableUsers = false;
+    users.ren = {
+        isNormalUser = true;
+        description = "ren";
+        extraGroups = [ "networkmanager" "wheel" ];
+        packages = with pkgs; [
+        #  thunderbird
+        ];
+        hashedPasswordFile = config.sops.secrets.machine-password.path;
+    };
+    users.root = {
+        hashedPasswordFile = config.sops.secrets.machine-password.path;
+    };
+};
 
-  # Enable automatic login for the user.
-  services.displayManager.autoLogin = {
-    enable = true;
-    user = "ren";
-  };
+# Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+systemd.services."getty@tty1".enable = false;
+systemd.services."autovt@tty1".enable = false;
+#}}}
 
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
+#{{{ Program Modules
+programs = {
+    firefox.enable = true;
+    neovim = {
+        enable = true;
+        defaultEditor = true;
+    };
+    fuse.userAllowOther = true;
+};
+#}}}
 
-  # Install firefox.
-  programs.firefox.enable = true;
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-  };
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+#{{{ Sys Packages
+environment.systemPackages = with pkgs; [
     git
     wget
     rclone
     age
     sops
-    #ddclient
     cloudflare-dyndns
     tailscale
-  ];
-  
-  programs.fuse.userAllowOther = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-
-
-  # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 53 ];
-  networking.firewall.allowedUDPPorts = [ 53 ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+    statix
+];
+#}}}
 
 }
--- flake.nix
+++ flake.nix
@@ -1,42 +1,41 @@
-{
-    description = "NixOS configuration";
+{ description = "NixOS configuration";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-        home-manager = {
-            url = "github:nix-community/home-manager";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-        sops-nix = {
-            url = "github:mic92/sops-nix";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+    sops-nix = {
+      url = "github:mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+};
 
-    outputs = inputs@{ nixpkgs, home-manager, sops-nix, ... }: {
-        nixosConfigurations = {
-            ren-laptop = nixpkgs.lib.nixosSystem {
-                system = "x86_64-linux";
-		specialArgs = {
-                     inherit inputs;
-		};
-                modules = [
-                    ./configuration.nix
-                    home-manager.nixosModules.home-manager {
-                        home-manager.useGlobalPkgs = true;
-                        home-manager.useUserPackages = true;
-                        home-manager.users.ren.imports = [
+outputs = inputs@{ nixpkgs, home-manager, sops-nix, ... }: {
+    nixosConfigurations = {
+        ren-laptop = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            specialArgs = { inherit inputs; };
+            modules = [
+                ./configuration.nix
+                home-manager.nixosModules.home-manager {
+                    home-manager = {
+                        useGlobalPkgs = true;
+                        useUserPackages = true;
+                        users.ren.imports = [
                             ./home.nix
-			    inputs.sops-nix.homeManagerModule
-			];
-			home-manager.extraSpecialArgs = { inherit inputs; };
-			home-manager.sharedModules = [
-                            sops-nix.homeManagerModules.sops
+                            inputs.sops-nix.homeManagerModule
                         ];
-                    }
-                    sops-nix.nixosModules.sops
-                ];
-            };
+                    };
+                    home-manager.extraSpecialArgs = { inherit inputs; };
+                    home-manager.sharedModules = [
+                        sops-nix.homeManagerModules.sops
+                    ];
+                }
+                sops-nix.nixosModules.sops
+            ];
         };
     };
+};
 }
