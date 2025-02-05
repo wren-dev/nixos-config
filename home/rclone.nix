c30c3175d72f5ab0e90534eb9962c923e9bc180f
Ren <renmain@proton.me>
2025-02-05 13:01:35 -0600

    Added a few rclone remotes

--- home/rclone.nix
+++ home/rclone.nix
@@ -5,6 +5,10 @@ in {
 
 home.activation.rclone = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     mkdir -p ${config.home.homeDirectory}/mnt/dropbox
+    mkdir -p ${config.home.homeDirectory}/mnt/mega
+    mkdir -p ${config.home.homeDirectory}/mnt/proton
+    mkdir -p ${config.home.homeDirectory}/mnt/gdrive
+
 '';
 
 sops.secrets.dropbox-token = {};
@@ -12,6 +16,8 @@ sops.secrets.mega-username = {};
 sops.secrets.mega-password = {};
 sops.secrets.proton-config = {};
 sops.secrets.gdrive-config = {};
+
+#{{{ Rclone.conf
 sops.templates."rclone.conf".content = ''
     [dropbox]
     type = dropbox
@@ -29,6 +35,9 @@ sops.templates."rclone.conf".content = ''
     ${config.sops.placeholder.gdrive-config}
 '';
 xdg.configFile."rclone/rclone.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.sops.templates."rclone.conf".path}";
+#}}}
+
+#{{{ rclone-dropbox.service
 systemd.user.services.rclone-dropbox = {
     Unit = {
         Description = "Rclone mount: dropbox";
@@ -49,8 +58,76 @@ systemd.user.services.rclone-dropbox = {
     Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
     };
 };
+#}}}
 
+#{{{ rclone-mega.service
+systemd.user.services.rclone-mega = {
+    Unit = {
+        Description = "Rclone mount: mega";
+        After = "network-online.target";
+        StartLimitInterval = 200;
+        StartLimitBurst = 2;
+    };
+    Install = {
+        WantedBy = [ "default.target" ];
+    };
+    Service = {
+        ExecStart = ''
+        ${pkgs.rclone}/bin/rclone mount --allow-other --vfs-cache-mode full --cache-dir ${config.home.homeDirectory}/.local/cache mega: ${config.home.homeDirectory}/mnt/mega
+        '';
+        ExecStop = ''
+            /run/wrappers/bin/fusermount -zu ${config.home.homeDirectory}/mnt/mega
+    '';
+    Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
+    };
+};
+#}}}
 
+#{{{ rclone-proton.service
+systemd.user.services.rclone-proton = {
+    Unit = {
+        Description = "Rclone mount: proton";
+        After = "network-online.target";
+        StartLimitInterval = 200;
+        StartLimitBurst = 2;
+    };
+    Install = {
+        WantedBy = [ "default.target" ];
+    };
+    Service = {
+        ExecStart = ''
+        ${pkgs.rclone}/bin/rclone mount --allow-other --vfs-cache-mode full --cache-dir ${config.home.homeDirectory}/.local/cache proton: ${config.home.homeDirectory}/mnt/proton
+        '';
+        ExecStop = ''
+            /run/wrappers/bin/fusermount -zu ${config.home.homeDirectory}/mnt/proton
+    '';
+    Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
+    };
+};
+#}}}
+
+#{{{ rclone-gdrive.service
+systemd.user.services.rclone-gdrive = {
+    Unit = {
+        Description = "Rclone mount: gdrive";
+        After = "network-online.target";
+        StartLimitInterval = 200;
+        StartLimitBurst = 2;
+    };
+    Install = {
+        WantedBy = [ "default.target" ];
+    };
+    Service = {
+        ExecStart = ''
+        ${pkgs.rclone}/bin/rclone mount --allow-other --vfs-cache-mode full --cache-dir ${config.home.homeDirectory}/.local/cache gdrive: ${config.home.homeDirectory}/mnt/gdrive
+        '';
+        ExecStop = ''
+            /run/wrappers/bin/fusermount -zu ${config.home.homeDirectory}/mnt/gdrive
+    '';
+    Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
+    };
+};
+#}}}
 
 
 }
